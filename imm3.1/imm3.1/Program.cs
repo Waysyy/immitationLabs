f75657ec540e9bc10ca489f9cee4287b21d20072
Waysy <annatkach225@gmail.com>
2022-12-07 20:46:29 +0700

    :bird:

---
+++ imm3.1/imm3.1/Program.cs
@@ -0,0 +1,61 @@
+﻿ 
+
+double normalDetail = 0;
+Random random = new Random();
+double profit = 0;
+
+for(int i = 0; i < 100000; ++i)
+{
+    double R1 = random.NextDouble();
+    if (R1 >= 0.2)
+    {
+        profit -= 25;
+        double R21 = random.NextDouble();
+        double R22 = random.NextDouble();
+        if (R21 > 0.03 && R22 >0.06)
+        {
+            profit -= 4;
+            profit += 35;
+            ++normalDetail;
+        }
+        if ((R21 < 0.03 && R22 > 0.06) || (R22 < 0.06 && R21 > 0.03))
+        {
+            profit -= 6;
+            profit += 35;
+            ++normalDetail;
+        }
+        if (R21 < 0.03 && R22 < 0.06)
+        {
+            profit -= 29;
+        }
+    }
+    if (R1 >= 0.08 && R1 < 0.2)
+    {
+        profit -= 28;
+        double R21 = random.NextDouble();
+        double R22 = random.NextDouble();
+        if (R21 > 0.03 && R22 > 0.06)
+        {
+            profit -= 4;
+            profit += 35;
+            ++normalDetail;
+        }
+        if ((R21 < 0.03 && R22 > 0.06) || (R22 < 0.06 && R21 > 0.03))
+        {
+            profit -= 6;
+            profit += 35;
+            ++normalDetail;
+        }
+        if (R21 < 0.03 && R22 < 0.06)
+        {
+            profit -= 29;
+        }
+    }
+    if (R1 < 0.08)
+    {
+        profit -= 25;
+    }
+}
+double fullProfit = profit / 100000;
+double probability = normalDetail / 100000;
+Console.WriteLine("Вероятность изготовления годной детали "+ probability + "\n"+ "Средняя прибыль за одну деталь = "+ fullProfit);
---
+++ imm3.2/imm3.2/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Text.RegularExpressions;
+
+Random random = new Random();
+double call = 1.2;
+double lenghtCall = 2;
+double p = call/lenghtCall;
+double p0 = 1 / (1 + p);
+double p1 = 1 - p0;
+double A = call * p0; 
+
+double allcall = 0;
+for (int i = 0; i < 100; i++)
+{
+    for (int j = 0; j < 8; j++)
+    {
+        for (int k = 0; k < 60; k++)
+        {
+            call = random.Next(1, 3);
+            allcall += call;
+        }
+    }
+}
+double resModel =allcall/(lenghtCall*60*8*100);
+double p00 = 1 / (1 + resModel);
+double p01 = 1 - p00;
+double A1 = (allcall/(60 * 8 * 100)) * p00;
+Console.WriteLine("Теоретические характеристики\n" + "Звонков в минуту: 1.2" + "\nСреднее число звонков:\n" + "Обслуженных: " + 
+    p0*100 +"%"+ "\nНе обслуженных: " + p1 * 100 +"%"+ "\nСредняя пропусная способность СМО: \n" +"Обслуживание звонков в минуту: " + A);
+
+Console.WriteLine("\nРезультаты имитации\n" + "Звонков в минуту: " + Math.Round(allcall / (60 * 8 * 100), 2) + "\nСреднее число звонков:\n" + "Обслуженных: " +
+   Math.Round(p00 * 100, 2) + "%" + "\nНе обслуженных: " + Math.Round(p01 * 100, 2) + "%" + "\nСредняя пропусная способность СМО: \n" + "Обслуживание звонков в минуту: " + Math.Round(A1,2));
